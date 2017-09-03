389f4f8954a2bc16e7146f10eebed01dfc5677d9
NSmeef <nathn.smith@gmail.com>
2017-09-03 17:02:36 +0100

    adding link to db, html for app to serve

--- stack/app/index.js
+++ stack/app/index.js
@@ -1,9 +1,12 @@
+const path = require("path");
 const express = require('express');
 const app = express();
-const redis = require('redis');
+const redis = require('redis'),
+    client = redis.createClient(6379, "db")
 
 app.get('/', function(req,res){
-    res.send('Hello');
+    res.sendFile(path.join(__dirname + '/www/index.html'));
+    console.log('got');
 })
 
 app.listen(3000, function() {
